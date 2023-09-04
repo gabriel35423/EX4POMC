afcf0430f67220ab9de099172d10ebe9c1acdaaf
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-09-04 19:04:11 -0300

    Add files via upload

---
+++ ProgramEx4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX4POOMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a base do triângulo: ");
+
+            Double baseValor = Double.Parse(Console.ReadLine());
+
+
+
+            Console.Write("Digite a altura do triângulo: ");
+
+            Double alturaValor = Double.Parse(Console.ReadLine());
+
+
+
+            Triangulo meuTriangulo = new Triangulo(baseValor, alturaValor);
+
+            Double area = meuTriangulo.CalcularArea();
+
+
+
+            Console.WriteLine("A área do triângulo é: {0}", area);
+        }
+    }
+}
